cdcf3db9b236c3a58d59844892393f49c938d2b7
Tapadyuti <maiti.t@husky.neu.edu>
2017-03-26 01:41:29 -0400

    on
    tw

---
+++ public/test.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Lenovo on 3/23/2017.
+ */
+console.log("Hello");
+var g;
+g = prompt("Put your number ");
+
+alert("Entered number "+ g);
+var i ;
+var arr = [];
+arr[0]= 1;
+arr[3]="33ffdfd";
+for(var i = 0 ; i < arr.length; i++ )
+{
+    console.log(arr[i]);
+}
